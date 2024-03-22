d606d29ae00185758a0d5f3a054ca023db6e1f71
平田裕梨絵 <>
2024-03-23 00:04:42 +0900

    フォローリスト/エラー

--- AtlasSNS/resources/views/follows/followList.blade.php
+++ AtlasSNS/resources/views/follows/followList.blade.php
@@ -4,5 +4,19 @@
 {{-- followList.blade.php --}}
 
 フォローリストページ
+{{-- フォローリストの表示 --}}
+    <div>
+        <h2>フォローしているユーザー</h2>
+        @if(!is_null($following) && count($following) > 0)
+            <ul>
+                @foreach($following as $username)
+                    <li>{{ $username>name }}</li>
+                @endforeach
+            </ul>
+        @else
+            <p>フォローしているユーザーはいません。</p>
+        @endif
+    </div>
+
 
 @endsection
