986c5bfd039522aca403ae3252255d9c800bc0fc
平田裕梨絵 <>
2024-03-23 10:16:48 +0900

    モーダル　作成中２

--- AtlasSNS/app/Http/Controllers/FollowsController.php
+++ AtlasSNS/app/Http/Controllers/FollowsController.php
@@ -13,19 +13,20 @@ class FollowsController extends Controller
     public function show(User $user, Follow $follow)
     {
         $login_user = auth()->user();
-        $is_following = $login_user->isFollowing($user->id);
-        // $is_followed = $login_user->isFollowed($user->id);
+        if ($login_user) {
+            $is_following = $login_user->isFollowing($user->id);
+        } else {
+            $is_following = false;
+        }
         $follow_count = $follow->getFollowCount($user->id);
         $follower_count = $follow->getFollowerCount($user->id);
 
         return view('follows.followList', [
             'user'           => $user,
             'is_following'   => $is_following,
-            // 'is_followed'    => $is_followed,
             'follow_count'   => $follow_count,
             'follower_count' => $follower_count
         ]);
-        // return view('follows.followList');
     }
 
     // フォロワーリストを表示
--- AtlasSNS/resources/views/follows/followList.blade.php
+++ AtlasSNS/resources/views/follows/followList.blade.php
@@ -3,19 +3,10 @@
 @section('content')
 {{-- followList.blade.php --}}
 
-フォローリストページ
 {{-- フォローリストの表示 --}}
     <div>
         <h2>フォローしているユーザー</h2>
-        @if(!is_null($following) && count($following) > 0)
-            <ul>
-                @foreach($following as $username)
-                    <li>{{ $username>name }}</li>
-                @endforeach
-            </ul>
-        @else
-            <p>フォローしているユーザーはいません。</p>
-        @endif
+
     </div>
 
 
