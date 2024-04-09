b3f73bea2f1e8c1bdd1280f4955ca9eb538bcd02
平田裕梨絵 <>
2024-04-09 16:06:04 +0900

    レイアウト調整

--- AtlasSNS/resources/views/users/profile.blade.php
+++ AtlasSNS/resources/views/users/profile.blade.php
@@ -2,14 +2,12 @@
 
 @section('content')
 
-<div class="container">
+<div id="container">
     @if ($errors->any())
     <div class="alert alert-danger">
-        <ul>
             @foreach ($errors->all() as $error)
-                <li>{{ $error }}</li>
+                <p>{{ $error }}</p>
             @endforeach
-        </ul>
     </div>
     @endif
 
